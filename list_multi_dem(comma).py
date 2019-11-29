d0ce608207dea73243eeb202d02072e8994822dd
SM <ss33ngmeht@gmail.com>
2019-11-29 01:35:35 +0100

    Hello

---
+++ list_multi_dem(comma).py
@@ -0,0 +1,33 @@
+groups = [
+["a","b","c","d"],
+["e","f","g","h"],
+["i","j","k","l"],
+]
+
+"""i=0
+j=0
+for word in group:
+    for one in word:
+        new = ', '.join(group[i][j])
+        j += 1
+        print (new)
+    i += 1 """
+
+#for group in groups:
+#    print(", ".join(group), end = ", ")
+
+print('\n')
+
+
+for group in groups.copy():
+    groups.remove(group)
+    print(", ".join(group), end="")
+    if groups != []:
+        print(end = ", ")
+
+
+print('\n')
+
+#for group in groups:
+#   print(", ".join(group))
+
